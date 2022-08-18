6be730d800ed52ba2d9e8a9ae471d3dcf28bc8e0
ecocaval <ecocaval@hotmail.com>
2022-08-18 19:17:42 -0300

    improving working code

--- optionalExercise2.js
+++ optionalExercise2.js
@@ -1,3 +1,19 @@
+/*
+    Troca as Vogais
+    Implemente a função ao lado que recebe uma string como parâmetro e retorna a string original, mas com o número 1 no lugar das vogais
+
+    Exemplo: se for passada a palavra “uva”, a função deve retornar “1v1”
+
+    Exemplo: se for passada a palavra “carro”, a função deve retornar “c1rr1”
+
+    Dica: para realizar essa questão você vai precisar de uma ferramenta nova. Talvez seja útil construir um array vazio. Você pode fazer isso com o seguinte comando:
+
+    const arr=[] //constrói um array vazio chamado "arr"
+    Depois disso, você, talvez, queira encher esse novo array com elementos. Você pode fazer isso escrevendo ".push" logo após o nome do array. Então, para inserir "3" no array que acabamos de criar fazemos:
+
+    arr.push(3) //Insere 3 dentro do array "arr"
+*/
+
 function trocaVogais(string) {
 
     const analisaVogal = (palavra, indiceLetra) => {
--- optionalExercise3.js
+++ optionalExercise3.js
@@ -1,3 +1,12 @@
+/*
+  Remover os Números
+  Implemente a função ao lado que recebe como parâmetro uma string, misturando letras e números, e deve retornar uma string contendo apenas as letras da string passada na mesma ordem
+
+  Exemplo: se for passada a string “ab2c4d”, a função deve retornar “abcd”
+
+  Exemplo: se for passada a string “1234”, a função deve retornar “”
+*/
+
 function tiraNumeros(string) {
 
     const analisaNumero = (palavra, indiceLetra) => {
